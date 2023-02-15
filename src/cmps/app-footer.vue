635b555608c2de84624a115b492dfc5f49a1f821
Yarden Zeron <zeron.yarden@gmail.com>
2023-02-15 15:29:04 +0200

    adding links to at the footer

--- src/cmps/app-footer.vue
+++ src/cmps/app-footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="full app-footer">
-        <h5>Created by <a href="linkedin.com/in/yarden-zeron-678247185">Yarden Zeron</a> <br/> collaborate with  <a href="http://coding-academy.org/">Coding Acadmey</a></h5>
+        <h5>Created by <a href="http://linkedin.com/in/yarden-zeron-678247185">Yarden Zeron</a> <br/> collaborate with  <a href="http://coding-academy.org/">Coding Acadmey</a></h5>
     </footer>
 </template>
 
